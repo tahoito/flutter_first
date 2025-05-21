12eb5bed70be472d31f3dc0adfef824084f71d13
tahoito <taho.ito0304@gmail.com>
2025-05-21 23:28:03 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text('First Widget App'),
+        ),
+        body: Center(
+          child: Text(
+            'Welcome to Flutter!',
+            style: TextStyle(fontSize:24),
+          ),
+        ),
+      ),
+    );
+  }
+}
