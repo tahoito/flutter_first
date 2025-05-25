ee673fa1eee41de0080509bf57d730905ef7ecce
tahoito <taho.ito0304@gmail.com>
2025-05-25 22:57:18 +0900

    edit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -7,14 +7,16 @@ import 'package:flutter_first/models/user.dart';
 import 'package:flutter_first/widgets/article_container.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({
+    super.key,
+  });
+
   @override
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
   List<Article> articles = [];
-  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,84 +25,54 @@ class _SearchScreenState extends State<SearchScreen> {
         title: const Text('Qiita Search'),
       ),
       body: Column(
-        children: [
-          // 検索ボックス
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 36),
-            child: TextField(
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-              decoration: const InputDecoration(
-                hintText: '検索ワードを入力してください',
-                border: OutlineInputBorder(),
-              ),
-              onSubmitted: (String value) async {
-                setState(() {
-                  isLoading = true;
-                });
-                final results = await searchQiita(value);
-                setState(() {
-                  articles = results;
-                  isLoading = false;
-                });
-              },
-            ),
-          ),
-          ArticleContainer(
-            article: Article(
-            title: 'テスト',
-            user: User(
-              id: 'qii-taro',
-              profileImageUrl: 'https://firebasestorage.googleapis.com/v0/b/gs-expansion-test.appspot.com/o/unknown_person.png?alt=media',
-            ),
-            createdAt: DateTime.now(),
-            tags: ['Flutter', 'dart'],
-            url: 'https://example.com',
-            ),
-          ),
-
-          // ローディング表示
-          if (isLoading)
-            const Padding(
-              padding: EdgeInsets.all(16),
-              child: CircularProgressIndicator(),
-            ),
-
-          // 検索結果一覧
-          if (!isLoading)
-            Expanded(
-              child: ListView.builder(
-                itemCount: articles.length,
-                itemBuilder: (context, index) {
-                  final article = articles[index];
-                  return ArticleContainer(article: article);
-                },
-              ),
-            ),
-        ],
+        children: <Widget>[
+      // 検索ボックス
+      Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 36,
       ),
+      child: TextField(
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.black,
+        ),
+        decoration: const InputDecoration(
+          hintText: '検索ワードを入力してください',
+          hintStyle: TextStyle(
+            fontSize: 16,
+            color: Colors.black54,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        onSubmitted: (value) async {
+          final result = await searchQiita(value);
+          setState(() => articles = result);
+        },
+      ),
+    ),
+    // 検索結果リストを追加
+    -         ArticleContainer(
+    -           article: Article(
+    -             title: 'テスト',
+    -             user: User(
+    -               id: 'qii-taro',
+    -               profileImageUrl: 'https://firebasestorage.googleapis.com/v0/b/gs-expansion-test.appspot.com/o/unknown_person.png?alt=media',
+    -             ),
+    -             createdAt: DateTime.now(),
+    -             tags: ['Flutter', 'dart'],
+    -             url: 'https://example.com',
+    -           ),
+    -         ),
+    +          Expanded(
+    +            child: ListView(
+    +              children: articles
+    +                  .map((article) => ArticleContainer(article: article))
+    +                  .toList(),
+    +            ),
+    +          ),
+    ],
+    ),
     );
-  }
-}
-
-// API通信部分
-Future<List<Article>> searchQiita(String keyword) async {
-  final uri = Uri.https('qiita.com', '/api/v2/items', {
-    'query': 'title:$keyword',
-    'per_page': '10',
-  });
-
-  final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
-  final http.Response res = await http.get(uri, headers: {
-    'Authorization': 'Bearer $token',
-  });
-
-  if (res.statusCode == 200) {
-    final List<dynamic> body = jsonDecode(res.body);
-    return body.map((dynamic json) => Article.fromJson(json)).toList();
-  } else {
-    return [];
   }
 }
